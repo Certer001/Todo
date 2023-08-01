1f5f53a6ad08c10f4ee657180e190c0fa3aa280a
Certer001 <76663981+Certer001@users.noreply.github.com>
2023-08-01 13:31:23 +0300

    patch 1.0.2

---
+++ js/form.js
@@ -0,0 +1,47 @@
+"use strict"
+//==========================================
+const TELEGRAM_BOT_TOKEN = '6536713689:AAFDWgcFMSf4L0SyG2Or6rGQe-GYjWq99Kk';
+const TELEGRAM_CHAT_ID = '@SentofMessage';
+const API = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+
+async function sendEmailTelegram(event) {
+    event.preventDefault();
+
+    const form = event.target;
+    const formButton = document.querySelector('.form__submit-button');
+    const fromSendResult = document.querySelector('.form__send-result');
+    fromSendResult.textContent = '';
+    
+    const { name, email, phone, pass } = Object.fromEntries(new FormData(form).entries());
+
+    const text = `Заявка от ${name}!\nEmail: ${email}\nТелефон: ${phone}\nПаспортные данные: ${pass}`;
+
+    try {
+        formButton.textContent = 'Loading...';
+        const response = await fetch(API, {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                chat_id: TELEGRAM_CHAT_ID,
+                text,
+            })
+          });
+          if(response.ok) {
+            fromSendResult.textContent = 'Спсибо за сообщение! мы скоро с вами свяжемся';
+            form.reset();
+          }
+          else {
+            throw new Error(response.statusText);
+          }
+    } catch (error) {
+        console.error(error);
+        fromSendResult.textContent = 'Сервис отправки сообщений не работает! Попробуйте позже';
+    }
+    finally{
+        formButton.textContent = 'Отправить';
+    }
+}
+
+/* const text = 'Заявка  от ' + name + '}!nEmail: ' + email + '/nТелефон: ' + phone +'/nПаспортные данные: ' + pass; */
